b4089dbec891c606b22566e11e05df7b0b6e17a9
Tintinnabulate <tintinn@abulate>
2018-09-24 20:38:29 +0100

    Remove syscalls

--- arch_windows.go
+++ arch_windows.go
@@ -2,15 +2,7 @@
 
 package gonfig
 
-import (
-	"golang.org/x/sys/windows"
-)
-
 // getTerminalWidth returns the width of the current terminal.
 func getTerminalWidth() int {
-	var info windows.ConsoleScreenBufferInfo
-	if err := windows.GetConsoleScreenBufferInfo(windows.Handle(0), &info); err != nil {
-		return 0
-	}
-	return int(info.Size.X)
+	return 80
 }
